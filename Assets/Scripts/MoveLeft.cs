a6ad06eef65097875688c6e74bffc04ef15a9193
PHURI001 <phuriwat.chat@gmail.com>
2026-02-27 10:48:44 +0700

    Complete

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -4,15 +4,26 @@ public class MoveLeft : MonoBehaviour
 {
     public float speed = 10f;
 
+    GameObject playerGameObject;
+    PlayerController player;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        // Find player by Tag
+        playerGameObject = GameObject.FindGameObjectWithTag("Player");
+        player = playerGameObject.GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime);
+        // Check IsGameOver
+        if (player.IsGameOver)
+        {
+            return;
+        }
+
+        transform.Translate(speed * Vector3.left * Time.deltaTime);
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,14 +4,27 @@ public class SpawnManager : MonoBehaviour
 {
     public Transform spawnPoint;
     public GameObject obstaclePrefab;
+    //public PlayerController Player;
 
     void Start()
     {
-
+        InvokeRepeating(nameof(Spawn), 0f, 2f);
     }
 
     void Spawn()
     {
+        //if (Player.IsGameOver)
+        //{
+        //    return;
+        //}
+
+        GameObject playerGameObject = GameObject.Find("Player");
+        PlayerController player = playerGameObject.GetComponent<PlayerController>();
+        if (player.IsGameOver)
+        {
+            return;
+        }
+
         Instantiate(
             obstaclePrefab,
             spawnPoint.position,
